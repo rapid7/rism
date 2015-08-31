31d59c068611afdd93a4b7e22073052597340ba3
tquetano-r7 <Tony_Quetano@rapid7.com>
2015-08-31 14:42:28 -0400

    Labels include project colors

--- dist/recess.js
+++ dist/recess.js
@@ -2772,7 +2772,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _variables2 = _interopRequireDefault(_variables);
 
-	exports["default"] = {
+	var _utils = __webpack_require__(15);
+
+	var _utils2 = _interopRequireDefault(_utils);
+
+	var labels = {
 	    label: (0, _reactStyleNormalizer2["default"])({
 	        backgroundColor: _variables2["default"].white.darker(0.4).toString(), // #ddd
 	        borderRadius: _variables2["default"].borderRadius * 2,
@@ -2783,7 +2787,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	        padding: 6,
 	        top: -2
 	    })
-	};
+	},
+	    projectColors = ["Danger", "Info", "Primary", "Success", "Warning"];
+
+	_utils2["default"].forEach(projectColors, function (color) {
+	    var fontColor = (color === "Warning" ? _variables2["default"].fontColor : _variables2["default"].white).toString();
+
+	    labels["labels" + color] = _utils2["default"].merge(_utils2["default"].clone(labels.label), {
+	        backgroundColor: _variables2["default"]["color" + color].toString(),
+	        color: fontColor
+	    });
+	});
+
+	exports["default"] = labels;
 	module.exports = exports["default"];
 
 /***/ },
--- src/labels.js
+++ src/labels.js
@@ -6,16 +6,35 @@
 
 import normalize from "react-style-normalizer";
 import variables from "./variables";
+import utils from "./utils";
 
-export default {
-    label:normalize({
-        backgroundColor:variables.white.darker(0.4).toString(), // #ddd
-        borderRadius:variables.borderRadius * 2,
-        display:"inline",
-        fontSize:variables.fontSize,
-        marginLeft:5,
-        marginRight:5,
-        padding:6,
-        top:-2
-    })
-};
+var labels = {
+        label:normalize({
+            backgroundColor:variables.white.darker(0.4).toString(), // #ddd
+            borderRadius:variables.borderRadius * 2,
+            display:"inline",
+            fontSize:variables.fontSize,
+            marginLeft:5,
+            marginRight:5,
+            padding:6,
+            top:-2
+        })
+    },
+    projectColors = [
+        "Danger",
+        "Info",
+        "Primary",
+        "Success",
+        "Warning"
+    ];
+
+utils.forEach(projectColors,function(color){
+    let fontColor = ((color === "Warning") ? variables.fontColor : variables.white).toString();
+
+    labels["labels" + color] = utils.merge(utils.clone(labels.label),{
+        backgroundColor:variables["color" + color].toString(),
+        color:fontColor
+    });
+});
+
+export default labels;
